66b79fe52350b0a191a787d8a29396ecee73c2e0
Martin Vanek <vanek@outlook.com>
2024-06-21 05:13:32 +0200

    Normschächte ausgelagert

---
+++ graphics_ns.py
@@ -0,0 +1,106 @@
+import ifcopenshell
+from utils import add_color, generate_guid, create_local_placement, create_cartesian_point, create_property_single_value
+
+def create_ifc_normschacht(ifc_file, ns, abwasserknoten, facility, context, default_durchmesser, default_hoehe, default_sohlenkote, default_wanddicke, default_bodendicke, abwasserknoten_group, einfaerben, data):
+    lage = abwasserknoten.get('lage', {})
+    x_mitte = float(lage.get('c1'))
+    y_mitte = float(lage.get('c2'))
+    breite = float(ns['dimension1']) / 1000.0 if ns['dimension1'] != '0' else default_durchmesser
+    tiefe = float(ns['dimension2']) / 1000.0 if ns['dimension2'] != '0' else default_hoehe
+    radius = breite / 2
+    hoehe = tiefe
+    wanddicke = default_wanddicke
+    bodendicke = default_bodendicke
+
+    kote = abwasserknoten.get('kote')
+    z_mitte = float(kote) if kote and float(kote) != 0 else default_sohlenkote
+
+    base_center = create_cartesian_point(ifc_file, (x_mitte, y_mitte, z_mitte))
+    axis = ifc_file.create_entity("IfcDirection", DirectionRatios=(0.0, 0.0, 1.0))
+    axis_placement = ifc_file.create_entity("IfcAxis2Placement3D", Location=base_center, Axis=axis)
+    ifc_local_placement = ifc_file.create_entity("IfcLocalPlacement", RelativePlacement=axis_placement)
+
+    if breite <= 2 * wanddicke:
+        profile = ifc_file.create_entity("IfcCircleProfileDef", ProfileType="AREA", Radius=radius)
+        body = ifc_file.create_entity("IfcExtrudedAreaSolid", SweptArea=profile, ExtrudedDirection=axis, Depth=hoehe)
+        items = [body]
+    else:
+        outer_profile = ifc_file.create_entity("IfcCircleProfileDef", ProfileType="AREA", Radius=radius)
+        outer_body = ifc_file.create_entity("IfcExtrudedAreaSolid", SweptArea=outer_profile, ExtrudedDirection=axis, Depth=hoehe)
+
+        inner_radius = radius - wanddicke
+        inner_profile = ifc_file.create_entity("IfcCircleProfileDef", ProfileType="AREA", Radius=inner_radius)
+        inner_body = ifc_file.create_entity("IfcExtrudedAreaSolid", SweptArea=inner_profile, ExtrudedDirection=axis, Depth=hoehe)
+
+        boolean_result = ifc_file.create_entity("IfcBooleanResult", Operator="DIFFERENCE", FirstOperand=outer_body, SecondOperand=inner_body)
+
+        bottom_profile = ifc_file.create_entity("IfcCircleProfileDef", ProfileType="AREA", Radius=inner_radius)
+        bottom_body = ifc_file.create_entity("IfcExtrudedAreaSolid", SweptArea=bottom_profile, ExtrudedDirection=axis, Depth=bodendicke)
+
+        bottom_placement = create_local_placement(ifc_file, [x_mitte, y_mitte, z_mitte - bodendicke])
+        
+        items = [boolean_result, bottom_body]
+
+    schacht = ifc_file.create_entity("IfcDistributionChamberElement",
+        GlobalId=generate_guid(),
+        Name=ns.get('bezeichnung', 'Normschacht'),
+        ObjectPlacement=ifc_local_placement,
+        Representation=ifc_file.create_entity("IfcProductDefinitionShape",
+            Representations=[ifc_file.create_entity("IfcShapeRepresentation",
+                ContextOfItems=context,
+                RepresentationIdentifier="Body",
+                RepresentationType="SweptSolid",
+                Items=items
+            )]
+        )
+    )
+    
+    properties = {
+        "Bezeichnung": ns.get('bezeichnung', ''),
+        "Standortname": ns.get('standortname', ''),
+        "Höhe": str(hoehe),
+        "Durchmesser": str(breite),
+        "Funktion": ns.get('funktion', ''),
+        "Material": ns.get('material', ''),
+        "Sohlenkote": str(z_mitte)
+    }
+
+    property_set = ifc_file.create_entity("IfcPropertySet",
+        GlobalId=generate_guid(),
+        Name="TBAKTZH STRE Schacht",
+        HasProperties=[create_property_single_value(ifc_file, key, value) for key, value in properties.items()]
+    )
+
+    ifc_file.create_entity("IfcRelDefinesByProperties",
+        GlobalId=generate_guid(),
+        RelatingPropertyDefinition=property_set,
+        RelatedObjects=[schacht]
+    )
+
+    ifc_file.create_entity("IfcRelContainedInSpatialStructure",
+        GlobalId=generate_guid(),
+        RelatedElements=[schacht],
+        RelatingStructure=facility
+    )
+
+    ifc_file.create_entity("IfcRelAssignsToGroup",
+        GlobalId=generate_guid(),
+        RelatedObjects=[schacht],
+        RelatingGroup=abwasserknoten_group
+    )
+
+    fehlende_werte = 0
+    farbe = "Blau"
+    if einfaerben:
+        if ns.get('dimorg1') == '0' or ns.get('dimorg2') == '0':
+            fehlende_werte += 1
+        if not abwasserknoten or float(abwasserknoten.get('kote', 0)) == 0:
+            fehlende_werte += 1
+
+        farbe = "Grün"
+        if fehlende_werte == 1:
+            farbe = "Orange"
+        elif fehlende_werte >= 2:
+            farbe = "Rot"
+
+    add_color(ifc_file, schacht, farbe, context)
